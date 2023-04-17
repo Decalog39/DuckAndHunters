9b9e3dc0710aa01e40ee2bb5671f85e537d780b7
Decalog39 <decalog39@gmail.com>
2023-04-17 15:10:27 +0200

    Added card visual (prototype) and fixed error

--- Assets/Scripts/Cards.cs
+++ Assets/Scripts/Cards.cs
@@ -20,10 +20,19 @@ public class Cards : MonoBehaviour
     public int hp;
     public int attack;
     public int id;
-    public string name;
+    public string cardName;
     public string description;
 
     public Cards(){
-        
+
+    }
+
+    public Cards(int cost, int hp, int attack, int id, string cardName, string description){
+        this.cost = cost;
+        this.hp = hp;
+        this.attack = attack;
+        this.id = id;
+        this.cardName = cardName;
+        this.description = description;
     }
 }
