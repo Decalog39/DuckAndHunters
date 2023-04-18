0ddc99d9d2c854dc5849e4bf3c8fb57e4539c80d
adammeh <adammehdi735@gmail.com>
2023-04-18 10:59:40 +0200

    CardController changes

---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardController : MonoBehaviour
+{
+
+    public Card card;
+    public TextMeshProUGUI cardName, cost, attack, health;
+
+    private void awake() {
+        Initialize(card);
+    }
+    
+    private void Start() {
+    }
+
+    public void Initialize(charCard card) {
+        cardName.text = card.cardName;
+        cost.text = card.text.ToString();
+        attack.text = card.attack.ToString();
+        health.text = card.health.ToString();
+    }
+
+    private void Update() {
+    }
+
+}
