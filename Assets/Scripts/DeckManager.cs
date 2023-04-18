452d780b1f2c1ce2da1b96d0ace3b53f7e468238
Ch15a <dagechen2003@gmail.com>
2023-04-18 11:09:30 +0200

    ok

--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class DeckManager : MonoBehaviour
 {
-    public List<Card> deck = new List<Card>(30);
+    public Card[] deck = new Card[30];
     
     void Start()
     {
         for (int i = 0; i < 15; i++){
-            deck[i] = (new CharCard(i, 0, 0, 0, null, null));
+            deck[3] = (new CharCard(3, 0, 0, 0, null, null));
             deck[15 + i] = (new SpellCard(15 + i, 0, null, null));
         }
     }
