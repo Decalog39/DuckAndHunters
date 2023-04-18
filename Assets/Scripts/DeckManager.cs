07e3a7742df959def009ae09986d6da1a3d0f8b6
Ch15a <dagechen2003@gmail.com>
2023-04-18 13:54:26 +0200

    oik

--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class DeckManager : MonoBehaviour
 {
-    public Card[] deck = new Card[30];
+    public List<Card> deck = new List<Card>();
     
     void Start()
     {
-        for (int i = 0; i < 15; i++){
-            deck[i] = (new Card(i, true, 0, 0, 0, null, null));
-            deck[15 + i] = (new Card(15 + i, false, 0, null, null, null, null));
-        }
+        
     }
+
+
 }
