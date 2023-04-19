ff8c71bd8da0c28252e4e92e34f19d24117d180a
adammeh <adammehdi735@gmail.com>
2023-04-19 08:34:56 +0200

    update cardDatabse

--- Assets/Scripts/CardDatabase.cs
+++ Assets/Scripts/CardDatabase.cs
@@ -26,27 +26,26 @@ public class CardDatabase : MonoBehaviour
         // cardList.Add(new Card(10,true, "Egg", 6, 2, 2, "This is an Egg"));
 
 
-        cardList.add(new Card(0, true, "None", 0, 0, 0, "None", 0));
+        cardList.Add(new Card(0, true, "None", 0, 0, 0, "None", 0));
 
         //hunters
-        cardList.add(new Card(1, true, "MasterHunter", 10, 6, 6, "This is a MasterHunter", 0));
-        cardList.add(new Card(2, true, "Hunter", 9, 5, 5, "This is a Hunter", 0));
-        cardList.add(new Card(3, true, "ApprenticeHunter", 8, 4, 4, "This is an ApprenticeHunter", 0));
-        cardList.add(new Card(4, true, "Caddy", 7, 3, 3, "This is a Caddy", 0));
-        cardList.add(new Card(5, true, "Nova Scotia Duck Tolling Retriever", 6, 2, 2, "This is a Nova Scotia Duck Tolling Retriever", 0));
+        cardList.Add(new Card(1, true, "MasterHunter", 10, 6, 6, "This is a MasterHunter", 0));
+        cardList.Add(new Card(2, true, "Hunter", 9, 5, 5, "This is a Hunter", 0));
+        cardList.Add(new Card(3, true, "ApprenticeHunter", 8, 4, 4, "This is an ApprenticeHunter", 0));
+        cardList.Add(new Card(4, true, "Caddy", 7, 3, 3, "This is a Caddy", 0));
+        cardList.Add(new Card(5, true, "Nova Scotia Duck Tolling Retriever", 6, 2, 2, "This is a Nova Scotia Duck Tolling Retriever", 0));
 
         //ducks
-        cardList.add(new Card(6, true, "KingDuck", 10, 6, 6, "This is a KingDuck", 0));
-        cardList.add(new Card(7, true, "CrownPrincessDuck", 9, 5, 5, "This is a CrownPrincessDuck", 0));
-        cardList.add(new Card(8, true, "SoldierDuck", 8, 4, 4, "This is a SoldierDuck", 0));
-        cardList.add(new Card(9, true, "Duck", 7, 3, 3, "This is a Duck", 0));
-        cardList.add(new Card(10, true, "Egg", 6, 2, 2, "This is an Egg", 0));
-
+        cardList.Add(new Card(6, true, "KingDuck", 10, 6, 6, "This is a KingDuck", 0));
+        cardList.Add(new Card(7, true, "CrownPrincessDuck", 9, 5, 5, "This is a CrownPrincessDuck", 0));
+        cardList.Add(new Card(8, true, "SoldierDuck", 8, 4, 4, "This is a SoldierDuck", 0));
+        cardList.Add(new Card(9, true, "Duck", 7, 3, 3, "This is a Duck", 0));
+        cardList.Add(new Card(10, true, "Egg", 6, 2, 2, "This is an Egg", 0));
 
         //spells
-        cardList.add(new Card(11, false, "Warmth", 4, 0, 0, "Egg's attack and health increase to duck's", 2));
-        cardList.add(new Card(12, false, "PowerBooster", 5, 0, 0, "Attack increases for 2", 3));
-        cardList.add(new Card(13, false, "Remedy", 5, 0, 0, "Health increases for 2", 3));
+        cardList.Add(new Card(11, false, "Warmth", 4, 0, 0, "Egg's attack and health increase to duck's", 2));
+        cardList.Add(new Card(12, false, "PowerBooster", 5, 0, 0, "Attack increases for 2", 3));
+        cardList.Add(new Card(13, false, "Remedy", 5, 0, 0, "Health increases for 2", 3));
 
 
     }
